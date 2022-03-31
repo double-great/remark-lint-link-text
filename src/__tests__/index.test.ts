db2ab6f1494a5c21a8badf07cb9fac8aa2a0f960
Jason Morris <jasonmorris@fastmail.com>
2022-03-30 20:27:54 -0400

    Documentation (#11)
    * Spelling fix
    
    * Overhaul readme
    
    * Reword tests
    
    * Add yes/no example to "not descriptive" section

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -21,19 +21,19 @@ describe("remark-lint-link-text", () => {
       dedent`
       # Title
 
-      A good link: Visit [Mapbox Documentation](https://docs.mapbox.com) for more infomration.
+      A good link: Visit [Example’s website](https://example.com) for more information.
 
-      A bad link: [click here](https://docs.mapbox.com).
+      A bad link: [click here](https://example.com).
 
-      A bad link: [link](https://google.com)
+      A bad link: [link](https://example.com)
     `
     );
 
     expect(lint.messages.length).toEqual(2);
     expect(lint.messages).toMatchInlineSnapshot(`
       Array [
-        [5:13-5:50: Avoid using the link text “click here,” it can be confusing when a screen reader reads it out of context. Replace it with a short description of the link’s destination.],
-        [7:13-7:39: Avoid using the link text “link,” it can be confusing when a screen reader reads it out of context. Replace it with a short description of the link’s destination.],
+        [5:13-5:46: Avoid using the link text “click here,” it can be confusing when a screen reader reads it out of context. Replace it with a short description of the link’s destination.],
+        [7:13-7:40: Avoid using the link text “link,” it can be confusing when a screen reader reads it out of context. Replace it with a short description of the link’s destination.],
       ]
     `);
   });
@@ -43,7 +43,7 @@ describe("remark-lint-link-text", () => {
       dedent`
       # Title
 
-      A bad link: [Click here](https://docs.mapbox.com).
+      A bad link: [Click here](https://example.com).
     `
     );
 
@@ -55,12 +55,12 @@ describe("remark-lint-link-text", () => {
 
   test("warns against url as link text", async () => {
     const lint = await processMarkdown(
-      dedent`A bad link: [https://github.com.com](https://github.com.com).`
+      dedent`A bad link: [https://example.com/initiatives/business/papers/important-ones.htm](https://example.com/initiatives/business/papers/important-ones.htm).`
     );
 
     expect(lint.messages.length).toEqual(1);
     expect(lint.messages[0].reason).toMatchInlineSnapshot(
-      `"Avoid using a URL “https://github.com.com” as the link text. Consider users who must speak it out loud and who must listen to a screen reader announce it. Replace it with a short description of the link’s destination."`
+      `"Avoid using a URL “https://example.com/initiatives/business/papers/important-ones.htm” as the link text. Consider users who must speak it out loud and who must listen to a screen reader announce it. Replace it with a short description of the link’s destination."`
     );
   });
 
@@ -69,7 +69,7 @@ describe("remark-lint-link-text", () => {
       dedent`
       # Title
 
-      A good link: [the Mapbox Isochrone API documentation](https://docs.mapbox.com).
+      A good link: [important business papers](https://example.com/initiatives/business/papers/important-ones.htm).
     `
     );
 
@@ -79,9 +79,9 @@ describe("remark-lint-link-text", () => {
   test("should pass", async () => {
     const lint = await processMarkdown(
       dedent`
-      ## Option 1: Choropleth
+      ## Example 1
       
-      In this option, we will create a choropleth visualization using data from [The Washington Post's "2ºC: Beyond the Limit" series about rising temperatures](https://github.com/washingtonpost/data-2C-beyond-the-limit-usa), which analyzes warming temperatures in the United States.
+      In this example we link to [important business papers](https://example.com/initiatives/business/papers/important-ones.htm) for more information.
     `
     );
     expect(lint.messages.length).toEqual(0);
@@ -89,47 +89,47 @@ describe("remark-lint-link-text", () => {
 
   test("unique link text", async () => {
     const lint = await processMarkdown(
-      dedent`Visit the [staff directory](https://www.directory.org) to learn more. You can visit the other [staff directory](https://www.other-directory.org) to learn other stuff.`
+      dedent`Visit the [staff directory](https://example.com/about-us/) to learn more. You can visit the other [staff directory](https://example.com/team/directory/) to learn other stuff.`
     );
     expect(lint.messages).toMatchInlineSnapshot(`
       Array [
-        [1:11-1:55: The link text “staff directory” is used more than once with different URLs. Change the link text to be unique to the URL.],
+        [1:11-1:59: The link text “staff directory” is used more than once with different URLs. Change the link text to be unique to the URL.],
       ]
     `);
   });
 
   test("link with image", async () => {
     const lint = await processMarkdown(
-      dedent`Visit the [staff directory ![](https://my-image.png)](https://www.directory.org).`
+      dedent`Visit the [staff directory ![](example.png)](https://example.com).`
     );
     expect(lint.messages).toMatchInlineSnapshot(`Array []`);
   });
 
   test("link is image, no alt text", async () => {
     const lint = await processMarkdown(
-      dedent`Visit the [![](https://my-image.png)](https://www.directory.org).`
+      dedent`Visit the [![](example.png)](https://example.com).`
     );
     expect(lint.messages).toMatchInlineSnapshot(`
       Array [
-        [1:11-1:65: The link “https://www.directory.org” must have link text or the image inside the link must have alt text],
+        [1:11-1:50: The link “https://example.com” must have link text or the image inside the link must have alt text],
       ]
     `);
   });
 
   test("link is image, alt text", async () => {
     const lint = await processMarkdown(
-      dedent`Visit the [![staff directory](https://my-image.png)](https://www.directory.org).`
+      dedent`Visit the [![staff directory](example.png)](https://example.com).`
     );
     expect(lint.messages).toMatchInlineSnapshot(`Array []`);
   });
 
   test("missing link text", async () => {
     const lint = await processMarkdown(
-      dedent`Visit the [](https://www.directory.org).`
+      dedent`Visit the [](https://example.com).`
     );
     expect(lint.messages).toMatchInlineSnapshot(`
       Array [
-        [1:11-1:40: The link “https://www.directory.org” must have link text],
+        [1:11-1:34: The link “https://example.com” must have link text],
       ]
     `);
   });
@@ -139,29 +139,29 @@ describe("remark-lint-link-text", () => {
       dedent`
       # Title
 
-      A bad link: [this mapbox article](https://docs.mapbox.com).
-      A bad link: [this Mapbox article](https://docs.mapbox.com).
-      A bad link: [this article](https://docs.mapbox.com).
-      A bad link: [this blog post](https://docs.mapbox.com).
-      A bad link: [the Mapbox blog post](https://docs.mapbox.com).
+      A bad link: [this example article](https://example.com).
+      A bad link: [this article](https://example.com).
+      A bad link: [click here](https://example.com).
+      A bad link: [this blog post](https://example.com).
+      A bad link: [the example blog post](https://example.com).
     `
     );
 
     expect(lint.messages.length).toEqual(5);
     expect(lint.messages[0].reason).toMatchInlineSnapshot(
-      `"Avoid using the link text “this mapbox article,” it can be confusing when a screen reader reads it out of context. Replace it with a short description of the link’s destination."`
+      `"Avoid using the link text “this example article,” it can be confusing when a screen reader reads it out of context. Replace it with a short description of the link’s destination."`
     );
     expect(lint.messages[1].reason).toMatchInlineSnapshot(
-      `"Avoid using the link text “this Mapbox article,” it can be confusing when a screen reader reads it out of context. Replace it with a short description of the link’s destination."`
+      `"Avoid using the link text “this article,” it can be confusing when a screen reader reads it out of context. Replace it with a short description of the link’s destination."`
     );
     expect(lint.messages[2].reason).toMatchInlineSnapshot(
-      `"Avoid using the link text “this article,” it can be confusing when a screen reader reads it out of context. Replace it with a short description of the link’s destination."`
+      `"Avoid using the link text “click here,” it can be confusing when a screen reader reads it out of context. Replace it with a short description of the link’s destination."`
     );
     expect(lint.messages[3].reason).toMatchInlineSnapshot(
       `"Avoid using the link text “this blog post,” it can be confusing when a screen reader reads it out of context. Replace it with a short description of the link’s destination."`
     );
     expect(lint.messages[4].reason).toMatchInlineSnapshot(
-      `"Avoid using the link text “the Mapbox blog post,” it can be confusing when a screen reader reads it out of context. Replace it with a short description of the link’s destination."`
+      `"Avoid using the link text “the example blog post,” it can be confusing when a screen reader reads it out of context. Replace it with a short description of the link’s destination."`
     );
   });
 });
