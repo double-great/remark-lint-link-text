7f48b5cba2d4ede185f5ccd469feebd4d0c987b4
Katy DeCorah <katydecorah@gmail.com>
2022-04-03 14:34:39 -0400

    Use classes

--- src/rules/email.ts
+++ src/rules/email.ts
@@ -1,17 +1,28 @@
 import { TextNode } from "../index.js";
+import Rule from "../rule.js";
 
-export default function checkEmail({
-  node,
-  text,
-}: {
-  node: TextNode;
-  text: string;
-}) {
-  const { url } = node;
-  if (!url || !url.startsWith("mailto:")) return;
-  const emailArray = url.split("?");
-  const email = emailArray[0].replace("mailto:", "");
-  if (!text.includes(email)) {
-    return `Text must include email “${email}” because the link URL will generate an email message`;
+class CheckEmail extends Rule {
+  constructor() {
+    super();
+    this.recommendation = this.setRecommendation();
+  }
+
+  check({ node, text }: { node: TextNode; text: string }) {
+    const { url } = node;
+    if (!url || !url.startsWith("mailto:")) return;
+    const emailArray = url.split("?");
+    const email = emailArray[0].replace("mailto:", "");
+    if (!text.includes(email)) {
+      this.recommendation = this.setRecommendation(email);
+      return this.suggestion();
+    }
+  }
+
+  setRecommendation(value?: string) {
+    return `Text must include email “${value}” because the link URL will generate an email message`;
   }
 }
+
+const checkEmail = new CheckEmail();
+
+export default checkEmail;
